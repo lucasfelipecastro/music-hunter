78be6d04d73da4c72f044de3094e78ac46df9e16
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-02-13 10:55:35 -0300

    feat: add UI/UX elements including footer and author text

--- main.py
+++ main.py
@@ -14,12 +14,22 @@ def widgets():
     image_path = os.path.join(r'C:\techWin11\pythonProjects\music-hunter\assets\logo.jpg')
 
     if os.path.exists(image_path):
-        st.image(image_path, width=300)
-    st.title('Music Hunter')
-
+        st.image(image_path)
+    
+    st.markdown(
+        """
+        <h1 style="font-family: 'Times New Roman', serif; color: rgb(10, 44, 28); text-align: center;">
+            Music Hunter
+        </h1>
+        
+        <p style="font-family: 'Times New Roman', serif; color: #555; text-align: center;">
+            Discover and explore your favorite song lyrics in seconds, like a hunter finding its prey!
+        </p>
+        """, unsafe_allow_html=True)
+                
 def inputs():
-    band = st.text_input('Enter the band name', key='band')
-    music = st.text_input('Enter the music name', key='music')
+    band = st.text_input('Enter the band name: ', key='band')
+    music = st.text_input('Enter the music name: ', key='music')
     return band, music
 
 def search_button(band, music):
@@ -33,7 +43,24 @@ def search_button(band, music):
         else:
             st.error('Lyrics not found')
 
+def footer():
+    st.markdown("""
+    <div style="text-align: center; margin-bottom: 10px;">
+        <p style="font-size: 16px; color: #555;">Developed by Lucas Felipe</p>
+    </div>
+                
+    <div style="display: flex; justify-content: center; gap: 20px;">
+        <a href="https://github.com/lucasfelipecastro" target="_blank">
+            <img src="https://img.icons8.com/?size=100&id=12599&format=png&color=000000" width="40" alt="GitHub">
+        </a>
+        <a href="https://www.linkedin.com/in/lucasfelipecastro" target="_blank">
+            <img src="https://img.icons8.com/?size=100&id=8808&format=png&color=000000" width="40" alt="LinkedIn">
+        </a>
+    </div>
+                """, unsafe_allow_html=True)
+
 if __name__ == '__main__':
     widgets()
     band, music = inputs()
     search_button(band, music)
+    footer()
